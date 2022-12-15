c1313d68d4e0a12b93f10be9af56e201dcc3ab50
Christian O'Leary <christian.oleary@mtu.ie>
2022-12-15 16:48:22 +0000

    Removed some binary files, added .gitignore, script to pull Python repositories for testing

---
+++ tests/python/pull_repos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Cloning repositories...
+
+# Clone repository or pull if already existing
+if cd ./tests/python/adanet; then echo Adanet: && git pull; else git clone https://github.com/tensorflow/adanet.git ./tests/python/adanet; fi
+cd ../../../
+if cd ./tests/python/autokeras; then echo AutoKeras: && git pull; else git clone https://github.com/keras-team/autokeras.git ./tests/python/autokeras; fi
+cd ../../../
+if cd ./tests/python/autogluon; then echo AutoGluon: && git pull; else git clone https://github.com/autogluon/autogluon.git ./tests/python/autogluon; fi
+cd ../../../
+if cd ./tests/python/Auto-PyTorch; then echo Auto-PyTorch: && git pull; else git clone https://github.com/automl/Auto-PyTorch.git ./tests/python/Auto-PyTorch; fi
+cd ../../../
+
+echo Script completed
