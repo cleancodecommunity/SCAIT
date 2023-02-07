0beed4f1200509808efcc93a235fc796ca5a05dc
Christian O'Leary <christian.oleary@mtu.ie>
2023-02-07 10:41:15 +0000

    Fix pull_repos.sh to pull multiple repos, added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# SCAIT
+
+Static code analysis tool. Overview in progress...
+
+## Usage
+
+In progress...
+
+## Run Tests
+
+Run tests using the following commands (commands vary slightly based on OS):
+
+```bash
+./tests/python/pull_repos.sh
+python -m pip install -r ./tests/python/requirements.txt
+python -m pytest ./tests/python/test_parsing.py
+```
+
+## Citing
+
+In progress...
--- tests/python/pull_repos.sh
+++ tests/python/pull_repos.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
+# Usage: sh ./tests/python/pull_repos.sh
+
 echo Cloning repositories...
 
 # Clone repository or pull if already existing
-if cd ./tests/python/adanet; then echo Adanet: && git pull; else mkdir -p ./tests/python/adanet && git clone https://github.com/tensorflow/adanet.git ./tests/python/adanet; fi
-cd ../../../
-if cd ./tests/python/autokeras; then echo AutoKeras: && git pull; else mkdir -p ./tests/python/autokeras && git clone https://github.com/keras-team/autokeras.git ./tests/python/autokeras; fi
-cd ../../../
-if cd ./tests/python/autogluon; then echo AutoGluon: && git pull; else mkdir -p ./tests/python/autogluon && git clone https://github.com/autogluon/autogluon.git ./tests/python/autogluon; fi
-cd ../../../
-if cd ./tests/python/Auto-PyTorch; then echo Auto-PyTorch: && git pull; else mkdir -p ./tests/python/Auto-PyTorch && git clone https://github.com/automl/Auto-PyTorch.git ./tests/python/Auto-PyTorch; fi
-cd ../../../
+if cd ./tests/python/adanet; then echo Adanet: && git pull && cd ../../../; else mkdir -p ./tests/python/adanet && git clone https://github.com/tensorflow/adanet.git ./tests/python/adanet; fi
+if cd ./tests/python/autokeras; then echo AutoKeras: && git pull && cd ../../../; else mkdir -p ./tests/python/autokeras && git clone https://github.com/keras-team/autokeras.git ./tests/python/autokeras; fi
+if cd ./tests/python/autogluon; then echo AutoGluon: && git pull && cd ../../../; else mkdir -p ./tests/python/autogluon && git clone https://github.com/autogluon/autogluon.git ./tests/python/autogluon; fi
+if cd ./tests/python/Auto-PyTorch; then echo Auto-PyTorch: && git pull && cd ../../../; else mkdir -p ./tests/python/Auto-PyTorch && git clone https://github.com/automl/Auto-PyTorch.git ./tests/python/Auto-PyTorch; fi
 
 echo Script completed
